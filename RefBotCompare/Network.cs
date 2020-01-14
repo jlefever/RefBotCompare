a203a0de8598dd8a190302efd936d72a605722ed
Jason Lefever <jtl86@drexel.edu>
2020-01-14 18:08:23 -0500

    Initial commit

---
+++ RefBotCompare/Network.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefBotCompare
+{
+    public class RunConfig
+    {
+        public IDictionary<string, string> Cookies { get; }
+        public string ProjectUrl { get; set; }
+    }
+
+    public class HttpClientFactory
+    {
+        public HttpClient CreateHttpClient(IDictionary<string, string> cookies)
+        {
+            var container = new CookieContainer();
+            
+            foreach(var cookie in cookies)
+            {
+                container.Add(new Cookie(cookie.Key, cookie.Value));
+            }
+
+            return new HttpClient(new HttpClientHandler() { CookieContainer = container });
+        }
+    }
+
+    public class CachedRefBotClient
+    {
+
+    }
+
+    public class RefBotClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public RefBotClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+    }
+
+    public interface ICache<T>
+    {
+        Task<T> TryGetAsync(string key, Func<Task<T>> loadFunc);
+    }
+
+    public class FileSystemCache : ICache<string>
+    {
+        private readonly string _directory;
+
+        public FileSystemCache(string directory)
+        {
+            _directory = directory;
+        }
+
+        public async Task<string> TryGetAsync(string key, Func<Task<string>> loadFunc)
+        {
+            var filename = key + ".txt";
+            var path = Path.Combine(_directory, filename);
+
+            if (File.Exists(path))
+            {
+                return File.ReadAllText(path);
+            }
+
+            if (!Directory.Exists(_directory))
+            {
+                Directory.CreateDirectory(_directory);
+            }
+
+            var text = await loadFunc();
+            File.WriteAllText(path, text);
+            return text;
+        }
+    }
+}
---
+++ RefBotCompare/Program.cs
@@ -0,0 +1,116 @@
+﻿using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace RefBotCompare
+{
+    public class Project
+    {
+        public IEnumerable<Solution> Solutions { get; }
+
+        public Project(IEnumerable<Solution> solutions)
+        {
+            Solutions = solutions;
+        }
+
+        public IDictionary<string, int> CountTotalMentions()
+        {
+            return CountFiles(Solutions.SelectMany(s => s.Files));
+        }
+
+        public IDictionary<string, int> CountUniqueMentions()
+        {
+            return CountFiles(Solutions.SelectMany(s => s.Files.Distinct()));
+        }
+
+        private static IDictionary<string, int> CountFiles(IEnumerable<string> files)
+        {
+            var counts = new Dictionary<string, int>();
+
+            foreach (var file in files)
+            {
+                counts.TryAdd(file, 0);
+                counts[file] = counts[file] + 1;
+            }
+
+            return counts;
+        }
+    }
+
+    public class Solution
+    {
+        public int SolutionId { get; }
+        public int ClusterId { get; }
+        public IEnumerable<string> Files { get; }
+
+        public Solution(int solutionId, int clusterId, IEnumerable<string> files)
+        {
+            SolutionId = solutionId;
+            ClusterId = clusterId;
+            Files = files;
+        }
+    }
+
+    public class RefBotDocument
+    {
+        private readonly IHtmlDocument _document;
+
+        public RefBotDocument(IHtmlDocument document)
+        {
+            _document = document;
+        }
+
+        public Project FindProject()
+        {
+            return new Project(FindSolutions());
+        }
+
+        public IEnumerable<Solution> FindSolutions()
+        {
+            var table = _document.GetElementById(TableId) as IHtmlTableElement;
+
+            foreach (var row in table.Rows.Skip(RowStartIndex))
+            {
+                var solutionIdText = row.Children[SolutionIdColIndex].TextContent;
+                var solutionId = Convert.ToInt32(solutionIdText);
+
+                var clusterIdText = row.Children[ClusterIdColIndex].TextContent;
+                var clusterId = Convert.ToInt32(clusterIdText);
+
+                var filesText = row.Children[FilesColIndex].TextContent;
+                var files = filesText.Split(',').Select(f => f.Trim());
+
+                yield return new Solution(solutionId, clusterId, files);
+            }
+        }
+
+        private const string TableId = "html_table_all_solutions";
+        private const int RowStartIndex = 1;
+        private const int SolutionIdColIndex = 0;
+        private const int ClusterIdColIndex = 2;
+        private const int FilesColIndex = 17;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var html = File.ReadAllText(@"C:\Users\jtl86\Desktop\refbot_apache_ant.html");
+
+            var document = new HtmlParser().ParseDocument(html);
+
+            var project = new RefBotDocument(document).FindProject();
+
+            var counts = project.CountUniqueMentions();
+
+            foreach (var count in counts.OrderByDescending(c => c.Value))
+            {
+                Console.WriteLine(count.Key + "\t\t\t" + count.Value);
+            }
+        }
+    }
+}
